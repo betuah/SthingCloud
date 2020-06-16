fbc0d3fc21c6911ad01dedeabc632c1189a25e03
Betuah Anugerah <betuahanugerah@gmail.com>
2020-06-16 12:27:43 +0700

    Remove semicolon

--- Server/routes/control_route.js
+++ Server/routes/control_route.js
@@ -1,39 +1,39 @@
-const authMiddleware = require('../middlewares/auth_middleware');
-const control = require('../controllers/control_controller');
+const authMiddleware = require('../middlewares/auth_middleware')
+const control = require('../controllers/control_controller')
 
 module.exports = (app) => {   
 
     app.route('/api/controller')
-        .get(authMiddleware, control.index);
+        .get(authMiddleware, control.index)
 
     app.route('/api/controller/:id')
-        .get(authMiddleware, control.findOne);
+        .get(authMiddleware, control.findOne)
 
     app.route('/api/controller')
-        .post(authMiddleware, control.create);
+        .post(authMiddleware, control.create)
     
     app.route('/api/controller/:id')
-        .put(authMiddleware, control.edit);
+        .put(authMiddleware, control.edit)
 
     app.route('/api/controller/widget_layouts/:id')
-        .put(authMiddleware, control.widget_layouts);
+        .put(authMiddleware, control.widget_layouts)
     
     app.route('/api/controller/default/:id')
-        .put(authMiddleware, control.defaultControl);
+        .put(authMiddleware, control.defaultControl)
 
     app.route('/api/controller') 
-        .delete(authMiddleware, control.delete);
+        .delete(authMiddleware, control.delete)
         
     app.route('/api/controller/widget/:controllerId')
-        .post(authMiddleware, control.widget_create);
+        .post(authMiddleware, control.widget_create)
     
     app.route('/api/controller/widget/:controllerId/:widgetId')
-        .put(authMiddleware, control.widget_update);
+        .put(authMiddleware, control.widget_update)
     
     app.route('/api/controller/widget/:controllerId/:widgetId')
-        .delete(authMiddleware, control.widget_delete);
+        .delete(authMiddleware, control.widget_delete)
 
     app.route('/api/controller/widgetData/:controllerId/:widgetId')
-        .put(authMiddleware, control.widgetData_update);
+        .put(authMiddleware, control.widgetData_update)
         
 };
