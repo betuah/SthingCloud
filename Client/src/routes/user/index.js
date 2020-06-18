2576b1e04e861a2f744e02a86e91d4bc9dcc34df
Betuah Anugerah <betuahanugerah@gmail.com>
2020-06-18 18:29:14 +0700

    Add header img cover

--- Client/src/routes/user/index.js
+++ Client/src/routes/user/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Redirect, Route, Switch } from "react-router-dom"
 import { withAuth } from 'components/Auth/context/AuthContext'
 import Profile from './components/Profile.js'
@@ -10,18 +10,25 @@ class User extends Component {
     }
     
     render() {
-        const { match } = this.props
-
+        const { match, location } = this.props
+        
         if(!this.props.isLoggedIn)
             return <Redirect push to='/user/signin' />
 
         return (
-            <div>
+            <Fragment>
+                <div className="text-center text-body-reverse" style={{position: 'relative', paddingTop: '20px', paddingBottom: '20px'}}>
+                    <div className="hero-bg-img" style={{backgroundColor: '#424242'}}></div>
+                    <div className="hero-bg-img" style={{opacity: 0.6, backgroundImage: 'url(assets/flat-images/nature_flat_03.jpg)'}}></div>
+                    <div className="custom-modal-title" style={{position: 'relative'}}>
+                        <h3>{location.pathname === `${match.url}/profile` ? 'User Profile' : 'Settings'}</h3>
+                    </div>
+                </div>
                 <Switch>
                     <Route exact path={`${match.url}/profile`} component={Profile} />
                     <Route exact path={`${match.url}/settings`} component={Settings} />
                 </Switch>
-            </div>
+            </Fragment>
         )
     }
 }
