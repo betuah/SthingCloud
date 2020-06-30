bf1be8655f5b5a6336604e8f917b41a047242067
Betuah Anugerah <betuahanugerah@gmail.com>
2020-06-30 22:56:19 +0700

    Change socket io function logic for device state status

--- SocketIO/app.js
+++ SocketIO/app.js
@@ -21,12 +21,9 @@ io.origins((origin, callback) => {
     }
 })
 
-
-/* Array device */
-let tmp = []
-
 io.on("connection", socket => {
     // console.log("New Client connected"); 
+    socket.on("connect", () => console.log("New client connected"))
 
     /* Create Room to each user connected */
     socket.on('join_room', room => {
@@ -39,89 +36,35 @@ io.on("connection", socket => {
 
     socket.on("graph_data", data => {
         // console.log(data)
-        console.log(data)
         io.sockets.in(data.idUser).emit(`${data.idDevice}-${data.type}`, {
             value: data.value
         });
         // console.log(io.sockets.adapter.rooms)
     })
 
-    /* Event to receive data from iot device */
+    /* Event to receive data from Mosca Server */
     socket.on("device_connect", data => {
-        const curentTime = moment() // Set current time
-        const tmpData = {
-            curentTime: curentTime,
-            ...data // Receive data from API device
-        }
+        let err = null
 
-        const isIndex = tmp.findIndex(e => e.idDevice && (e.idDevice === data.idDevice)) // Get index of device in array
-        if(isIndex !== -1) { // If device found in array
-            tmp[isIndex].curentTime = curentTime // Will set curent time to existing array
-        } else { // Else device not found in array
-
-            let err = 0 // Variable for error status
-
-            tmp.push(tmpData) // Adding new device to array
-
-            /* Change state to 1 mean connected */
-            iotDeviceModel.findByIdAndUpdate({ _id: data.idDevice }, { $set: { state: 1 }})
-            .catch(error => {
-                err = 1
-                console.log("Something error in device_connect Function SocketIO")
-                console.log(error)
-            })
-
-            /* Get count device connected in spesific userID */           
-            getDeviceConn = tmp.filter(val => val.idUser ? (val.idUser.match(data.idUser) ? val : '') : '')
-
-            /* Tell client that device status was change */
-            io.sockets.in(data.idUser).emit('event', {
-                statusChange: 1,
-                device: data.deviceName,
-                onlineDevice: getDeviceConn.lenght,
-                error: err
-            });     
-        }
-    })
-
-    /* Check each second for get device time out infomation */
-    setInterval(() => { 
-        const intervalCurentTime = moment() // Set current time
-        
-        tmp.forEach((item, index) => { // Check every index in array
-            const timeOut = moment.duration(intervalCurentTime.diff(item.curentTime)).asMinutes() // Get device time out
-            if (timeOut > 1) { // Check that the device has timed out not more than 1 minute
-
-                let err = 0
-                
-                /* Change status to 0 mean disconnected */
-                iotDeviceModel.findByIdAndUpdate({ _id: item.idDevice }, { $set: { state: 0 }})
-                .catch(error => {
-                    err = 1
-                    console.log("Some thing error in setInterval Function SocketIO")
-                    console.log(error)
-                })
-
-                /* Get count device connected in spesific userID */
-                getDeviceConn = tmp.filter(val => val.idUser ? (val.idUser.match(item.idUser) ? val : '') : '')
-
-                /* Tell client that device status was change */
-                io.sockets.in(item.idUser).emit('event', {
-                    statusChange: 0,
-                    device: item.deviceName,
-                    onlineDevice: getDeviceConn.lenght,
-                    error: err
-                });   
-                
-                /* Remove offline device from array */
-                tmp.splice(index, 1)
-            }
+        // Change state to 1 mean connected 
+        // Update device status in database
+        iotDeviceModel.findByIdAndUpdate({ _id: data.idDevice }, { $set: { state: data.deviceStatus }})
+        .catch(error => {
+            // Handle if update to database error
+            err = error // Set error to err variable
+            console.log("Something error in device_connect Function SocketIO", error) // Show error log in console
         })
-    }, 1000)
 
-    /* A special namespace "disconnect" for when a client disconnects */
-    socket.on("disconnect", () => console.log("Client disconnected"))
-});
+        console.log(data.idUser)
+
+        /* Tell client that device status was change */
+        io.sockets.in(data.idUser.split('.')[0]).emit('event', {
+            statusChange: data.deviceStatus,
+            device: data.deviceName,
+            error: err
+        })
+    })
+})
 
 /* MongoDB Connection Check */
 const conn = require('./config/db_mongoDB')
